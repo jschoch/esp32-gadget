b6aeb21e9524ecfc5916dac5b921d41bde821064
jesse schoch <you@example.com>
2019-08-16 16:55:51 -0700

    words

--- gen_token.py
+++ gen_token.py
@@ -3,7 +3,7 @@
 import sys
 
 import hashlib
-def generate_token(endpoint_id, device_token):
+def generate_token(endpoint_id, device_secret):
     hash_object = hashlib.sha256(
         bytes(endpoint_id, 'utf-8') + bytes(device_token, 'utf-8')
     )
@@ -11,7 +11,7 @@ def generate_token(endpoint_id, device_token):
     return bytes(hex_dig, 'utf-8')
 
 if __name__ == "__main__":
-    endpoint_id, device_token = sys.argv[1], sys.argv[2]
+    endpoint_id, device_secret = sys.argv[1], sys.argv[2]
     print("endpoint id: \"{}\"".format(endpoint_id))
-    print("device token: \"{}\"".format(device_token))
-    print(generate_token(endpoint_id, device_token))
+    print("device token: \"{}\"".format(device_secret))
+    print(generate_token(endpoint_id, device_secret))
